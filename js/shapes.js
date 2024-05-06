15bcf3a1e2aeb188f94a8ba107a77b3059f3d815
patzhu <patzhu@umich.edu>
2024-05-05 20:56:08 -0700

    smaller cube

--- js/shapes.js
+++ js/shapes.js
@@ -1,5 +1,7 @@
 function renderShapes() {
     drawRectPrism([800, 0, -100], [1000, 400, 100]);
 
-    drawRectPrism([850, 0, 200], [900, 50, 250]);
+    drawRectPrism([875, 0, 200], [925, 50, 250]);
+
+    drawRectPrism([850, 150, -200], [950, 250, -300]);
 }
